cf36fc3bf9998d9352313607ad5a2e55b45f797c
Eyad Mohamed <feroo@gbas.168.1.7>
2021-10-31 13:08:23 +0200

    updating our app to redirect to home if user is signed in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,32 +1,31 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
+import { auth } from '../../firebase/firebase.utils';
 
-import { auth } from "../../firebase/firebase.utils";
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-import { ReactComponent as Logo } from "../../assets/crown.svg";
+import './header.styles.scss';
 
-import "./header.styles.scss";
-
-const Header = ({ currentuser }) => (
-  <div className="header">
-    <Link className="logo-container" to="/">
-      <Logo className="logo" />
+const Header = ({ currentUser }) => (
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
     </Link>
-    <div className="options">
-      <Link className="option" to="/shop">
+    <div className='options'>
+      <Link className='option' to='/shop'>
         SHOP
       </Link>
-      <Link className="option" to="/shop">
+      <Link className='option' to='/shop'>
         CONTACT
       </Link>
-      {currentuser ? (
-        <div className="option" onClick={() => auth.signOut()}>
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
           SIGN OUT
         </div>
       ) : (
-        <Link className="option" to="/signin">
+        <Link className='option' to='/signin'>
           SIGN IN
         </Link>
       )}
@@ -34,9 +33,8 @@ const Header = ({ currentuser }) => (
   </div>
 );
 
-
 const mapStateToProps = state => ({
-  currentuser: state.user.currentuser
-})
+  currentUser: state.user.currentUser
+});
 
 export default connect(mapStateToProps)(Header);
